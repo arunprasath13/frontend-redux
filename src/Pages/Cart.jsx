33bdfa814eed0fd335581ef4bbdb43a7979ebb7e
Arun Prasath <arunprasath41d@gmail.com>
2024-05-14 12:33:06 +0530

    Resolved some bugs

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import "./Cart.css"
 const Cart = () => {
+  
   const cart = useSelector(state => state.cart);
 
   return (
     <div className='cart'>
+      <h2></h2>
       <h2 className='cart__header'>Cart</h2>
       <ul className='cart__nav__links'>
         {cart.map((item, index) => (
