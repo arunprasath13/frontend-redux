9ec9362fe9265955b137646d11a5087976c9136b
Arun Prasath <arunprasath41d@gmail.com>
2024-05-15 10:11:35 +0530

    Fixed some bugs

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,25 +1,38 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import "./Cart.css"
+import React from "react";
+import { useSelector } from "react-redux";
+import "./Cart.css";
+import Buy from "../components/Buy";
+import Success from "../components/Success";
 const Cart = () => {
-  
-  const cart = useSelector(state => state.cart);
-
+  const cart = useSelector((state) => state.cart);
+  const total = cart.reduce((acc, item) => acc + (item ? item.price : 0), 0);
   return (
-    <div className='cart'>
-      <h2></h2>
-      <h2 className='cart__header'>Cart</h2>
-      <ul className='cart__nav__links'>
-        {cart.map((item, index) => (
-          <li key={index}>
-            {item ? `${item.title} - $${item.price}` : 'Invalid item'}
-          </li>
-        ))}
-      </ul>
+    <div className="cart">
+      <div style={{ display: "flex", justifyContent: "space-between",alignItems:"center",justifyContent:"center" }}>
+        <h1>Shopping Cart</h1>
+      </div>
+      <div style={{display:"flex",justifyContent: "space-between",alignItems:"center",justifyContent:"center",gap:"40px"
+      }}>
+        {cart.map((product) => {
+          return (
+            <div style={{border:"1px solid black",padding:"20px"}}>
+              <div className="img" style={{}}>
+                <img src={product.image} />
+              </div>
+              <div className="cart__info">
+                <h2>{product.title}</h2>
+                <div className="">
+                  <p>{product.price}</p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <h1>total: ${total.toFixed(2)}</h1>
+      {total > 0 && <Buy />}
     </div>
   );
 };
 
 export default Cart;
-
-
